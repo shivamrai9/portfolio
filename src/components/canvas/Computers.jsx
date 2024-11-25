bdc82f1dc570216f4f1fad8092525d52f9d47f80
shivamrai9 <Shivamrai5476@gmail.com>
2024-11-25 15:36:10 +0530

    Computer render issue

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -4,7 +4,7 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
 import CanvasLoader from "../Loader";
 
-const Computers = () => {
+const Computers = ({ isMobile }) => {
   const computer = useGLTF("./desktop_pc/scene.gltf");
 
   return (
@@ -14,20 +14,16 @@ const Computers = () => {
         position={[-20, 50, 10]}
         angle={0.12}
         penumbra={1}
-        intensity={1}
+        intensity={2}
         castShadow
         shadow-mapSize={512}
       />
-      {/* <pointLight intensity={0.2} /> */}
+      <pointLight intensity={0.2} />
       <primitive
         object={computer.scene}
-        // scale={isMobile ? 0.3 : 0.45}
-        scale={2.5}
-        // scale={isMobile ? 0.7 : 0.75}
-        position={ [0, -2.2, 0]} // Adjusted positions to center
-        // position={isMobile ? [0, -3, -2.2] : [0, -3.2, -1.5]} // Adjusted positions to center
-        // rotation={[-0.01, -0.2, -0.1]} // Adjusted rotation to center the object
-        rotation-y={0}
+        scale={isMobile ? 0.3 : 0.45}
+        position={isMobile ? [0, -1, 0] : [0, -2.2, 0]} // Adjusted positions to center
+        rotation={[-0.01, 0, 0]} // Adjusted rotation to center the object
       />
     </mesh>
   );
@@ -63,12 +59,12 @@ const ComputersCanvas = () => {
       frameloop='demand'
       shadows
       dpr={[1, 2]}
-      camera={{ position: [20, 3, 5], fov: 25 }}
+      // camera={{ position: [20, 3, 5], fov: 25 }}
+      camera={{
+        position: isMobile ? [5, 3, 5] : [20, 3, 5],  // Adjusted for mobile
+        fov: 25,
+      }}
       gl={{ preserveDrawingBuffer: true }}
-      // camera={{
-      //   position: isMobile ? [5, 3, 5] : [20, 3, 5],  // Adjusted for mobile
-      //   fov: 25,
-      // }}
     >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
@@ -76,7 +72,7 @@ const ComputersCanvas = () => {
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
-        <Computers  />
+        <Computers isMobile={isMobile} />
       </Suspense>
 
       <Preload all />
@@ -85,7 +81,3 @@ const ComputersCanvas = () => {
 };
 
 export default ComputersCanvas;
-
-
-
-
