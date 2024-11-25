1877fdcaaad5540215b32670fdc8555da77f9068
shivamrai9 <Shivamrai5476@gmail.com>
2024-11-25 15:28:15 +0530

    new model

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -4,7 +4,7 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
 import CanvasLoader from "../Loader";
 
-const Computers = ({ isMobile }) => {
+const Computers = () => {
   const computer = useGLTF("./desktop_pc/scene.gltf");
 
   return (
@@ -22,10 +22,12 @@ const Computers = ({ isMobile }) => {
       <primitive
         object={computer.scene}
         // scale={isMobile ? 0.3 : 0.45}
+        scale={2.5}
         // scale={isMobile ? 0.7 : 0.75}
-        // position={isMobile ? [0, -1, 0] : [0, -2.2, 0]} // Adjusted positions to center
-        position={isMobile ? [0, -3, -2.2] : [0, -3.2, -1.5]} // Adjusted positions to center
-        rotation={[-0.01, -0.2, -0.1]} // Adjusted rotation to center the object
+        position={ [0, -2.2, 0]} // Adjusted positions to center
+        // position={isMobile ? [0, -3, -2.2] : [0, -3.2, -1.5]} // Adjusted positions to center
+        // rotation={[-0.01, -0.2, -0.1]} // Adjusted rotation to center the object
+        rotation-y={0}
       />
     </mesh>
   );
@@ -62,11 +64,11 @@ const ComputersCanvas = () => {
       shadows
       dpr={[1, 2]}
       camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
       // camera={{
       //   position: isMobile ? [5, 3, 5] : [20, 3, 5],  // Adjusted for mobile
       //   fov: 25,
       // }}
-      gl={{ preserveDrawingBuffer: true }}
     >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
@@ -74,7 +76,7 @@ const ComputersCanvas = () => {
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
-        <Computers isMobile={isMobile} />
+        <Computers  />
       </Suspense>
 
       <Preload all />
